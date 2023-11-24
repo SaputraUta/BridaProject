f4f407a7b9ed7fc326dec4a875a4aadf4ca55f14
Saputra <saputra.uta50@gmail.com>
2023-11-24 18:04:29 +0800

    Add responsive profile, riwayat reservasi user

--- src/pages/customer/profiles/index.tsx
+++ src/pages/customer/profiles/index.tsx
@@ -8,59 +8,64 @@ export default function index() {
   const password = "inirahasia";
   return (
     <LayoutCustomer>
-      <div className="mx-24 mt-32">
+      <div className="mt-28 mx-5 sm:mt-32 md:mt-36 sm:mx-10 md:mx-16 lg:mx-24">
         <NavUser />
         <div className="mt-5 border-t-2 shadow-md rounded-xl">
           <div className="p-5">
             <div className="flex justify-between">
-              <h4 className="font-medium text-lg">Profile</h4>
+              <h4 className="font-bold text-xs sm:text-sm md:text-base lg:text-lg">Profile</h4>
               <button
                 type="button"
-                className="font-medium text-lg text-red-600"
+                className="font-medium text-xs sm:text-sm md:text-base lg:text-lg text-red-600"
               >
                 Edit
               </button>
             </div>
             <div className="flex flex-col gap-5 mt-5">
-                <div>
-                    <Image src='/userlogo.svg' alt="user" width={100} height={100} />
-                </div>
+              <div>
+                <Image
+                  src="/userlogo.svg"
+                  alt="user"
+                  width={100}
+                  height={100}
+                />
+              </div>
               <div>
                 <p
-                  className="font-semibold text-base text-gray-600
+                  className="font-semibold text-xs sm:text-sm md:text-base lg:text-lg text-gray-600
                 "
                 >
                   Username
                 </p>
-                <p className="text-base">@Saputrauta</p>
+                <p className="text-xs sm:text-sm md:text-base lg:text-lg">@Saputrauta</p>
               </div>
               <div>
                 <p
-                  className="font-semibold text-base text-gray-600
+                  className="font-semibold text-xs sm:text-sm md:text-base lg:text-lg text-gray-600
                 "
                 >
                   Nomor Telepon
                 </p>
-                <p className="text-base">082340767473</p>
+                <p className="text-xs sm:text-sm md:text-base lg:text-lg">082340767473</p>
               </div>
               <div>
                 <p
-                  className="font-semibold text-base text-gray-600
+                  className="font-semibold text-xs sm:text-sm md:text-base lg:text-lg text-gray-600
                 "
                 >
                   Email
                 </p>
-                <p className="text-base">082340767473</p>
+                <p className="text-xs sm:text-sm md:text-base lg:text-lg">082340767473</p>
               </div>
               <div>
                 <p
-                  className="font-semibold text-base text-gray-600
+                  className="font-semibold text-xs sm:text-sm md:text-base lg:text-lg text-gray-600
                 "
                 >
                   Password
                 </p>
                 <div className="flex gap-5 items-center">
-                  <p className="text-base">
+                  <p className="text-xs sm:text-sm md:text-base lg:text-lg">
                     {showPassword ? password : password.replace(/./g, "*")}
                   </p>
                   <Image
--- src/pages/customer/profiles/riwayat.tsx
+++ src/pages/customer/profiles/riwayat.tsx
@@ -4,24 +4,24 @@ import LayoutCustomer from "@/layout/layout-customer";
 export default function riwayat() {
   return (
     <LayoutCustomer>
-      <div className="mx-24 mt-32">
+      <div className="mt-28 mx-5 sm:mt-32 md:mt-36 sm:mx-10 md:mx-16 lg:mx-24">
         <NavUser />
         <div className="mt-5 border-t-2 shadow-md rounded-xl">
           <div className="p-5">
-            <h4 className="font-medium text-lg">Riwayat reservasi</h4>
+            <h4 className="font-bold text-xs sm:text-sm md:text-base lg:text-lg">Riwayat reservasi</h4>
             <div className="flex flex-col gap-5">
               <div className="flex justify-between items-center mt-2">
                 <div>
                   <p
-                    className="font-semibold text-base text-to
+                    className="font-semibold text-xs sm:text-sm md:text-base lg:text-lg text-to
                 "
                   >
                     Kamis 12 Desember 2023
                   </p>
-                  <p className="text-base">Taman Sangkareang</p>
+                  <p className="text-xs sm:text-sm md:text-base lg:text-lg">Taman Sangkareang</p>
                 </div>
                 <button
-                  className="font-semibold text-base text-to
+                  className="font-semibold text-xs sm:text-sm md:text-base lg:text-lg text-to
                 "
                 >
                   Lihat invoice
@@ -30,15 +30,15 @@ export default function riwayat() {
               <div className="flex justify-between items-center">
                 <div>
                   <p
-                    className="font-semibold text-base text-to
+                    className="font-semibold text-xs sm:text-sm md:text-base lg:text-lg text-to
                 "
                   >
                     Selasa 16 Maret 2023
                   </p>
-                  <p className="text-base">Taman Sangkareang</p>
+                  <p className="text-xs sm:text-sm md:text-base lg:text-lg">Taman Sangkareang</p>
                 </div>
                 <button
-                  className="font-semibold text-base text-to
+                  className="font-semibold text-xs sm:text-sm md:text-base lg:text-lg text-to
                 "
                 >
                   Lihat invoice
@@ -47,15 +47,15 @@ export default function riwayat() {
               <div className="flex justify-between items-center">
                 <div>
                   <p
-                    className="font-semibold text-base text-to
+                    className="font-semibold text-xs sm:text-sm md:text-base lg:text-lg text-to
                 "
                   >
                     Rabu 09 Agustus 2023
                   </p>
-                  <p className="text-base">Taman Sangkareang</p>
+                  <p className="text-xs sm:text-sm md:text-base lg:text-lg">Taman Sangkareang</p>
                 </div>
                 <button
-                  className="font-semibold text-base text-to
+                  className="font-semibold text-xs sm:text-sm md:text-base lg:text-lg text-to
                 "
                 >
                   Lihat invoice
