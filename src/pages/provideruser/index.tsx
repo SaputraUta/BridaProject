4546e498038537eb26713b94cc0a3d9f2dcba003
Saputra <saputra.uta50@gmail.com>
2023-11-25 13:21:08 +0800

    Dynamic rendering venue list provider dashboard

--- src/pages/provideruser/index.tsx
+++ src/pages/provideruser/index.tsx
@@ -1,14 +1,21 @@
 import LayoutProvider from "@/layout/layout-provider";
-import Link from "next/link";
 import NavProvider from "@/components/component-provider/NavProvider";
-import VenueList from "@/components/component-provider/VenueList";
+import dynamic from "next/dynamic";
+
+const DynamicVenueList = dynamic(
+  () => import("@/components/component-provider/VenueList"),
+  {
+    loading: () => <p className="text-xl font-bold text-center">Loading...</p>,
+    ssr: false,
+  }
+);
 
 export default function index() {
   return (
     <LayoutProvider>
       <div className="mt-24 mx-5 sm:mt-32 md:mt-36 sm:mx-10 md:mx-16 lg:mx-24">
         <NavProvider />
-        <VenueList />
+        <DynamicVenueList />
       </div>
     </LayoutProvider>
   );
