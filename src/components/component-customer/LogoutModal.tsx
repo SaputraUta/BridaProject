95ca7e9b2cc272853ca4c4760b11317a675d4d9e
Saputra <saputra.uta50@gmail.com>
2023-12-22 22:37:37 +0800

    logout event to delete cookie

--- src/components/component-customer/LogoutModal.tsx
+++ src/components/component-customer/LogoutModal.tsx
@@ -1,4 +1,6 @@
+import axios from "axios";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 interface LogoutModal {
@@ -7,28 +9,40 @@ interface LogoutModal {
 }
 
 export default function LogoutModal({ isOpen, onClose }: LogoutModal) {
-    useEffect(() => {
-        if (isOpen) {
-          document.body.style.overflow = "hidden";
-        } else {
-          document.body.style.overflow = "visible";
-        }
-      }, [isOpen]);
+  const router = useRouter();
+  async function handleLogout(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    const response = await fetch("http://localhost:3000/api/logout");
+    router.push("/login/customerlogin");
+  }
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "visible";
+    }
+  }, [isOpen]);
   return (
     <div>
       {isOpen ? (
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-70 z-50">
           <div className="bg-white rounded-xl w-fit h-fit flex flex-col items-center justify-center gap-10 p-5">
-            <h3 className="text-lg sm:text-xl md:text-2xl">Apakah anda yakin ingin Logout?</h3>
+            <h3 className="text-lg sm:text-xl md:text-2xl">
+              Apakah anda yakin ingin Logout?
+            </h3>
             <div className="flex gap-5">
-              <Link
-                href="/login/customerlogin"
-                className="bg-green-600 text-white rounded-2xl w-20 flex justify-center items-center"
+              <button onClick={handleLogout} className="bg-green-600 text-white rounded-2xl w-20 flex justify-center items-center">
+                <p className="py-3 px-5 text-center text-sm sm:text-base md:text-lg">
+                  Ya
+                </p>
+              </button>
+              <button
+                onClick={onClose}
+                className="bg-red-600 text-white rounded-2xl w-20 flex justify-center items-center"
               >
-                <p className="py-3 px-5 text-center text-sm sm:text-base md:text-lg">Ya</p>
-              </Link>
-              <button onClick={onClose} className="bg-red-600 text-white rounded-2xl w-20 flex justify-center items-center">
-                <p className="py-3 px-5 text-center text-sm sm:text-base md:text-lg">Tidak</p>
+                <p className="py-3 px-5 text-center text-sm sm:text-base md:text-lg">
+                  Tidak
+                </p>
               </button>
             </div>
           </div>
--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    res.setHeader("Set-Cookie", "token=; HttpOnly; path=/; Max-Age=1; SameSite=Lax; Secure");
+    res.setHeader("Set-Cookie", "token=; HttpOnly; path=/; Max-Age=-1; SameSite=Lax; Secure");
   
     res.status(200).json({
       message: 'Logout berhasil',
