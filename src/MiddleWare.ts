c9c19e98cdefae1fb1400d5fe90703ffb2c08555
Saputra <saputra.uta50@gmail.com>
2024-02-07 15:53:18 +0800

    Feat: add middleware based on role

--- src/MiddleWare.ts
+++ src/MiddleWare.ts
@@ -1,21 +1,102 @@
+import { jwtVerify } from "jose";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest){
-    const role = request.cookies.get("role");
-    const token = request.cookies.get("token");
-    if (request.nextUrl.pathname.startsWith("/admin")){
-        if (!token){
-            if (!role) return NextResponse.redirect(new URL("/", request.url));
-        }
-    }
+export async function middleware(req: NextRequest) {
+  const tokenCookie = req.cookies.get("token");
+  const token = tokenCookie?.value as string;
 
-    if (request.nextUrl.pathname.startsWith("/login")){
-        if (token){
-            if(role) return NextResponse.redirect(new URL("/", request.url));
+  if (req.nextUrl.pathname.startsWith("/admin")) {
+    if (!token) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    } else {
+      try {
+        const { payload } = await jwtVerify(
+          token,
+          new TextEncoder().encode("kinguta")
+        );
+        if (payload.role !== "Admin") {
+          return NextResponse.redirect(new URL("/login", req.url));
         }
-    }   
+      } catch (error) {
+        return NextResponse.redirect(new URL("/login", req.url));
+      }
+    }
+  }
+
+  if (req.nextUrl.pathname.startsWith("/customer")) {
+    if (!token) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    } else {
+      try {
+        const { payload } = await jwtVerify(
+          token,
+          new TextEncoder().encode("kinguta")
+        );
+        console.log("Decoded Token:", payload);
+        if (payload.role !== "Customer") {
+          return NextResponse.redirect(new URL("/login", req.url));
+        }
+      } catch (error) {
+        console.log(error);
+        return NextResponse.redirect(new URL("/login", req.url));
+      }
+    }
+  }
+
+  if (req.nextUrl.pathname.startsWith("/provideruser")) {
+    if (!token) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    } else {
+      try {
+        const { payload } = await jwtVerify(
+          token,
+          new TextEncoder().encode("kinguta")
+        );
+        if (payload.role !== "Provider") {
+          return NextResponse.redirect(new URL("/login", req.url));
+        }
+      } catch (error) {
+        return NextResponse.redirect(new URL("/", req.url));
+      }
+    }
+  }
+
+  if (
+    req.nextUrl.pathname.startsWith("/login") ||
+    req.nextUrl.pathname.startsWith("/auth") ||
+    req.nextUrl.pathname === "/"
+  ) {
+    if (token) {
+      try {
+        const { payload } = await jwtVerify(
+          token,
+          new TextEncoder().encode("kinguta")
+        );
+        if (payload.role === "Admin") {
+          return NextResponse.redirect(new URL("/admin", req.url));
+        }
+        if (payload.role === "Provider") {
+          return NextResponse.redirect(new URL("/provideruser", req.url));
+        }
+        if (payload.role === "Customer") {
+          return NextResponse.redirect(new URL("/customer", req.url));
+        }
+      } catch (error) {
+        console.log(error);
+        return NextResponse.redirect(new URL("/", req.url));
+      }
+    }
+  }
 }
+
 export const config = {
-    metcher: ["/admin/:path*", "/"],
+  matcher: [
+    "/admin/:path*",
+    "/customer/:path*",
+    "/provideruser/:path*",
+    "/login/:path*",
+    "/auth/:path*",
+    "/:path*",
+  ],
 };
