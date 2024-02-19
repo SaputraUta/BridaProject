24d8b708b4990f99c1cb77509bc2742af89680df
Saputra <saputra.uta50@gmail.com>
2024-02-19 20:50:55 +0800

    Feat: get booking list provider

---
+++ src/pages/api/Provider/transaction.ts
@@ -0,0 +1,80 @@
+import { type NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/lib/prisma";
+import jwt from "jsonwebtoken";
+
+async function handleGetMethod(req: NextApiRequest, res: NextApiResponse) {
+  console.log("REQUEST");
+  if (typeof req.cookies.token === "undefined") {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+  const user = jwt.verify(req.cookies.token, "kinguta") as {
+    id: number;
+    username: string;
+    email: string;
+    role: string;
+  };
+  console.log(user.id);
+
+  try {
+    const result = await prisma.transaksi.findMany({
+      where: {
+        prov_Id: user.id,
+      },
+    });
+    res.status(200).json(result);
+    console.log(result);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Terjadi kesalahan saat mengambil data, coba lagi nanti",
+    });
+  }
+}
+
+async function handlePostMethod(req: NextApiRequest, res: NextApiResponse) {
+  const data = req.body;
+  if (typeof req.cookies.token === "undefined") {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+  const user = jwt.verify(req.cookies.token, "kinguta") as {
+    id: number;
+    username: string;
+    email: string;
+    role: string;
+  };
+  console.log(user.id);
+  console.log(data.room_Id);
+
+  try {
+    const result = await prisma.transaksi.create({
+      data: {
+        nama_room: data.nama_room,
+        nama_venue: data.nama_venue,
+        tgl_booking: data.tgl_booking,
+        venue_id: Number(data.venue_id),
+        cust_Id: user.id,
+        prov_Id: Number(data.prov_Id),
+        room_Id: Number(data.room_Id),
+      },
+    });
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Terjadi kesalahan saat menyimpan data",
+    });
+  }
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    handlePostMethod(req, res);
+  }
+  if (req.method === "GET") {
+    handleGetMethod(req, res);
+  }
+}
--- src/pages/provideruser/bookingConfirmation.tsx
+++ src/pages/provideruser/bookingConfirmation.tsx
@@ -1,16 +1,77 @@
 import NavProvider from "@/components/component-provider/NavProvider";
 import OrderList from "@/components/component-provider/OrderList";
 import LayoutProvider from "@/layout/layout-provider";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface transaction {
+  id: number;
+  tgl_booking: string;
+  nama_room: string;
+  nama_venue: string;
+  room_Id: number;
+  venue_id: number;
+  prov_Id: number;
+  cust_Id: number;
+  is_approved: boolean;
+}
 
 export default function BookingConfirmation() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState("");
+  const [transactions, setTransactions] = useState<transaction[]>();
+
+  async function fetchData() {
+    console.log("REQ");
+    setIsLoading(true);
+    setMessage("");
+    try {
+      const response = await axios.get(
+        "http://localhost:3000/api/provider/transaction"
+      );
+      setIsLoading(false);
+      setTransactions(response.data);
+    } catch (err: any) {
+      setIsLoading(false);
+      setMessage(err.response.data.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <LayoutProvider>
       <div className="mt-24 mx-5 sm:mt-32 md:mt-36 sm:mx-10 md:mx-16 lg:mx-24">
-      <NavProvider />
-        <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-slate-800 mt-5">Order List</h2>
-        <OrderList/>
-        <OrderList/>
-        <OrderList/>
+        <NavProvider />
+        <div className="mt-5 border-t-2 shadow-md rounded-xl">
+          <div className="p-5">
+            <h2 className="font-bold text-sm sm:text-base md:text-lg lg:text-xl mb-2">
+              Order List
+            </h2>
+            <div className="flex flex-col gap-5">
+              {isLoading && (
+                <p className="text-slate-900 text-center font-medium text-xs sm:text-sm md:text-base lg:text-lg">
+                  Loading transaction data...
+                </p>
+              )}
+              {transactions?.map((transaction) => (
+                <OrderList
+                  id={transaction.id}
+                  tgl_booking={transaction.tgl_booking}
+                  nama_room={transaction.nama_room}
+                  nama_venue={transaction.nama_venue}
+                  room_Id={transaction.room_Id}
+                  venue_id={transaction.venue_id}
+                  prov_Id={transaction.prov_Id}
+                  cust_Id={transaction.cust_Id}
+                  is_approved={transaction.is_approved}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </LayoutProvider>
   );
